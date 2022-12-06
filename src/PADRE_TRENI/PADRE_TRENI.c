ecf243759d79ee790d4aba455bcf62016e5087d4
Andrea <andreatotigit@gmail.com>
2022-12-06 15:55:17 +0100

    Change PADRE TRENI
    * Change header for PADRE TRENI
    * Change implementation of PADRE TRENI

--- src/PADRE_TRENI/PADRE_TRENI.c
+++ src/PADRE_TRENI/PADRE_TRENI.c
@@ -5,30 +5,52 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <stdbool.h>
+#include <sys/wait.h>
 #include <unistd.h>
-#define DEFAULT_PROTOCOL 0
+#include <sys/file.h>
 
-int creaSegmento(char *nome) {
+static int file_create(char *filename);
+
+int process_create() {
+  int pid = fork();
+  if (pid < 0) {
+    perror("Error creating a child process.");
+    exit(EXIT_FAILURE);
+  }
+  return pid;
+}
+
+ static int file_create(char *filename) {
   int fd;
-  if ((fd = open(nome, O_RDWR | O_CREAT | O_TRUNC, 0666)) < 0) {
-    perror("errore durante l'apertura di un segmento");
+  if ((fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0666)) < 0) {
+    char err_string[30];
+    sprintf(err_string,"Error opening file %s", filename);
+    perror(err_string);
     exit(EXIT_FAILURE);
   }
   return fd;
 }
 
-void inizializzaSegmento(int fd) {
-  if (write(fd, "0", 1) != 1) {
-    perror("errore durante la scrittura in un segmento");
-    exit(EXIT_FAILURE);
-  }
+void segment_create(char* file,char* project_path,int seg_number){
+    sprintf(file, "%s/tmp/MA%d", project_path, seg_number + 1);
+    int segment = file_create(file);
+    file_write(segment, "0", 1);
+    close(segment);
 }
 
-int creaTreno() {
-  int pid = fork();
-  if (pid < 0) {
-    perror("errore durante le creazione di un processo treno");
+
+int file_write(int fd, char *msg, ssize_t msg_len) {
+  int bytesWritten = write(fd, msg, msg_len);
+  if (bytesWritten != msg_len) {
+    perror("Error writing into a file.");
     exit(EXIT_FAILURE);
   }
-  return pid;
+  return bytesWritten;
 }
+
+long file_length(int fd) { return lseek(fd, 0, SEEK_END); }
+
+
+
+
