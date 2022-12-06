abb3e06aac26936fcec3fc2d36e47983d03ca6f4
Andrea <andreatotigit@gmail.com>
2022-12-06 15:55:17 +0100

    Change PADRE TRENI
    * Change header for PADRE TRENI
    * Change implementation of PADRE TRENI

--- src/PADRE_TRENI/PADRE_TRENI.h
+++ src/PADRE_TRENI/PADRE_TRENI.h
@@ -1,10 +1,36 @@
+#include <stdbool.h>
+#include <sys/types.h>
 
-/* Semplice funzione che esegue una fork. Serve per creare i processi treni*/
-int creaTreno();
+/**
+ * @brief Create a child process using a fork system call.
+ *
+ * @return int Pid of the newly created process.
+ */
+int process_create();
 
-/* Crea un file che rappresenta un segmento dell'itinerario */
-int creaSegmento(char *);
+/**
+ * @brief Write the string msg to file descriptor fd .
+ *
+ * @param fd File descriptor
+ * @param msg String to write
+ * @param msg_len Length of the string msg
+ * @return int Number of bytes written.
+ */
+int file_write(int fd, char *msg, ssize_t msg_len);
 
-/* scrive il carattere '0' nel file riferito dal file descriptor passato come
- * parametro*/
-void inizializzaSegmento(int);
+/**
+ * @brief Compute the number of characters in a file.
+ *
+ * @param fd File descriptor
+ * @return long Length of the file
+ */
+long file_length(int fd);
+
+/**
+ * @brief Create a file representing a segment and initialize it with 0.  
+ * 
+ * @param file Pathname of segment 
+ * @param project_path Absolute pathname of project folder 
+ * @param seg_number Segment number 
+ */
+void segment_create(char *file, char *project_path, int seg_number);
