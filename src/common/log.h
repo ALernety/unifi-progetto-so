80ddb3a58fef03f7756a227e4b68743a45edcf5d
Andrea <andreatotigit@gmail.com>
2022-12-07 10:16:06 +0100

    Add Log
    * Add headers for Log
    * Add implementation of Log
    * Move logging functionality  from TRENO to Log

---
+++ src/common/log.h
@@ -0,0 +1,16 @@
+/**
+ * @brief Log current date and time in format DD/MM/YYYY hh/mm/ss 
+ * 
+ * @param fd Log file descriptor
+ */
+void log_current_date(int fd);
+
+
+/**
+ * @brief Log info about next or current segment
+ * 
+ * @param fd Log file descriptor 
+ * @param segment Segment name
+ * @param flag It is 0 if logging next segment, 1 if logging current segment  
+ */
+void log_segment(int fd, char *segment, int flag);
