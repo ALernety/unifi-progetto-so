4e9fb54f3dd302f86268af02a787938f6a6c5d96
Aliaksei Shapiolkin <alernety@gmail.com>
2022-12-20 13:11:50 +0100

    Refactor and format log

--- src/common/log.c
+++ src/common/log.c
@@ -1,51 +1,46 @@
 #include "log.h"
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
 
 void log_current_date(int fd) {
-  char buffer[30];
+  char log_message[30];
   size_t date_size;
-  struct tm tim;
+  struct tm current_time;
   time_t now;
 
   now = time(NULL);
-  tim = *(localtime(&now));
-  date_size = strftime(buffer, 30, "%d/%m/%Y; %H:%M:%S\n", &tim);
-  int bytesWritten = write(fd, buffer, date_size);
-  if (bytesWritten != (int)date_size) {
-    perror("Error logging date.");
-    exit(EXIT_FAILURE);
+  current_time = *(localtime(&now));
+  date_size = strftime(log_message, 30, "%d/%m/%Y; %H:%M:%S\n", &current_time);
+  int write_length = write(fd, log_message, date_size);
+  if (write_length != (int)date_size) {
+    perror("log date.");
+    abort();
   }
 }
 
- void log_segment(int fd, char *segment, int flag) {
-  char buffer[50];
-  
-  if (flag) {
-    sprintf(buffer,"%s %s%s ", "[Attuale:",segment,"] ");
-  } else {
-   sprintf(buffer,"%s %s%s", "[Next:",segment,"] ");
-  }
-  int writtenChars = write(fd, buffer, strlen(buffer));
-  if (writtenChars != (int)strlen(buffer)) {
-    perror("Error logging into file");
-    exit(EXIT_FAILURE);
+void log_segment(int fd, char *segment, bool is_current) {
+  const char *format_string = is_current ? "[Attuale: %s] " : "[Next: %s] ";
+  char log_message[strlen(format_string) + strlen(segment) + 1];
+
+  sprintf(log_message, format_string, segment);
+  int write_length = write(fd, log_message, strlen(log_message));
+  if (write_length != (int)strlen(log_message)) {
+    perror("log segment");
+    abort();
   }
 }
 
-int log_create(char *logFile) {
-  int fd = open(logFile, O_CREAT | O_WRONLY | O_TRUNC, 0666);
+int log_create(char *log_file) {
+  int fd = open(log_file, O_CREAT | O_WRONLY | O_TRUNC, 0644);
   if (fd == -1) {
-    perror("Error creating log file");
-    exit(EXIT_FAILURE);
+    perror("log create");
+    abort();
   }
   return fd;
-
 }
--- src/common/log.h
+++ src/common/log.h
@@ -1,24 +1,26 @@
+#include <stdbool.h>
+
 /**
- * @brief Log current date and time in format DD/MM/YYYY hh/mm/ss 
- * 
+ * @brief Log current date and time in format DD/MM/YYYY hh/mm/ss
+ *
  * @param fd Log file descriptor
  */
 void log_current_date(int fd);
 
-
 /**
  * @brief Log info about next or current segment
- * 
- * @param fd Log file descriptor 
+ *
+ * @param fd Log file descriptor
  * @param segment Segment name
- * @param flag It is 0 if logging next segment, 1 if logging current segment  
+ * @param is_current It is 0 if logging next segment, 1 if logging current
+ * segment
  */
-void log_segment(int fd, char *segment, int flag);
+void log_segment(int fd, char *segment, bool is_current);
 
 /**
  * @brief Create log file.
- * 
- * @param logFile Pathname of file 
+ *
+ * @param logFile Pathname of file
  * @return int File descriptor of created file
  */
-int log_create(char* logFile);
+int log_create(char *logFile);
