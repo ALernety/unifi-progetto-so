c3f9fdebbacf061b8df3d204a3af8c8cbfe771e3
Aliaksei Shapiolkin <alernety@gmail.com>
2022-12-22 15:41:28 +0100

    Add help string to PADRE_TRENI and refactor
    * Add arguments to create_process_train and traverse_itinerary
    * Update railway_manager to respect new arguments of PADRE_TRENI

--- src/PADRE_TRENI/main.c
+++ src/PADRE_TRENI/main.c
@@ -1,6 +1,7 @@
 #include "../PADRE_TRENI/PADRE_TRENI.h"
 #include "../common/alloc_macro.h"
 #include "../common/parent_dir.h"
+#include <arpa/inet.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,8 +15,72 @@
 #define TRAIN_NUM 5
 
 int main(int argc, char *argv[]) {
+  char help_str[650];
   char *ip_address = strdup("127.0.0.1");
   size_t port = 43210;
+  char *rbc_socket_file = strdup("tmp/rbc");
+  const char *request_delim = ",";
+
+  sprintf(help_str,
+          "\033[31mNot enough arguments! Example of use:\033[0m\n"
+          "\n"
+          "Usage: %s <AF_UNIX_SOCKET> <AF_INET_ADDRESS> <AF_INET_PORT> "
+          "<REQUEST_DELIMITER>\n\n"
+          "\033[36m<AF_UNIX_SOCKET>\033[0m possible values are:\n"
+          "    \033[36mtmp/rbc\033[0m        - Path to RBC socket. To use "
+          "without RBC set to empty string.\n"
+          "\n"
+          "\033[36m<AF_INET_ADDRESS>\033[0m possible values are:\n"
+          "    \033[36m127.0.0.1\033[0m      - Is default value\n"
+          "\n"
+          "\033[36m<AF_INET_PORT>\033[0m possible values are:\n"
+          "    \033[36m43210\033[0m          - Is default value\n"
+          "\n"
+          "\033[36m<REQUEST_DELIMITER>\033[0m possible values are:\n"
+          "    \033[36m,\033[0m              - Is default value\n",
+          argv[0]);
+
+  switch (argc) {
+  case 5:
+    request_delim = argv[4];
+    __attribute__((fallthrough));
+  case 4:
+    port = atoi(argv[3]);
+    __attribute__((fallthrough));
+  case 3:
+    ip_address = strdup(argv[2]);
+    __attribute__((fallthrough));
+  case 2: {
+    rbc_socket_file = strdup(argv[1]);
+    struct sockaddr_in sa;
+    bool is_wrong_ip = inet_pton(AF_INET, ip_address, &(sa.sin_addr)) == 0;
+    if (is_wrong_ip) {
+      printf("\033[31mWrong ip!\033[0m\n");
+      printf("%s", help_str);
+      exit(EXIT_FAILURE);
+    }
+    if (port == 0) {
+      printf("\033[31mWrong port!\033[0m\n");
+      printf("%s", help_str);
+      exit(EXIT_FAILURE);
+    }
+    bool is_rbc =
+        strcmp(rbc_socket_file, "") && access(rbc_socket_file, F_OK) == -1;
+    if (is_rbc) {
+      printf("\033[31mRBC socket file not reachable!\033[0m\n");
+      usleep(1000000);
+      if (access(rbc_socket_file, F_OK) == -1) {
+        printf("\033[31mRBC socket file not found!\033[0m\n");
+        printf("%s", help_str);
+        exit(EXIT_FAILURE);
+      }
+    }
+    break;
+  }
+  default:
+    printf("%s", help_str);
+    exit(EXIT_FAILURE);
+  }
 
   parent_dir_def(project_path, argv[0], 2);
   if (chdir(project_path) == -1) {
@@ -32,7 +97,7 @@ int main(int argc, char *argv[]) {
   }
   // create and execute train processes
   for (size_t train_index = 0; train_index < TRAIN_NUM; train_index++) {
-    create_train_process(train_index, ip_address, port);
+    create_train_process(train_index, ip_address, port, rbc_socket_file);
   }
   for (int i = 0; i < TRAIN_NUM; i++) {
     wait(NULL);
