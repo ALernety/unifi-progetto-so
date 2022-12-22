0a008ed97394754c85994a0ebda550d8baadec0e
Aliaksei Shapiolkin <alernety@gmail.com>
2022-12-22 11:20:18 +0100

    Bug fix railway, not changes with move_to_next_platform
    * Changed pointers of platforms to pointers to array of platforms where it should be
    * Correct documentation comments

--- src/RBC/platform.c
+++ src/RBC/platform.c
@@ -3,8 +3,8 @@
 #include "../common/string_handlers.h"
 #include <string.h>
 
-Platform set_platform_from(Platform *platform, char *platform_string,
-                           const char *delim, char **connected_ids) {
+Platform *set_platform_from(Platform *platform, char *platform_string,
+                            const char *delim, char **connected_ids) {
   char **platform_values_list = get_malloc_token_list(platform_string, delim);
 
   if (sizeof(platform_values_list) < 5) {
@@ -17,13 +17,13 @@ Platform set_platform_from(Platform *platform, char *platform_string,
   platform->actual_capacity = get_integer_from(platform_values_list[2]);
   platform->full_capacity = get_integer_from(platform_values_list[3]);
   *connected_ids = platform_values_list[4];
-  return *platform;
+  return platform;
 }
 
-Platform get_platform_by_id(Platform *platform_list, size_t platform_number,
-                            char *id) {
+Platform *get_platform_by_id(Platform **platform_list, size_t platform_number,
+                             char *id) {
   for (size_t index = 0; index < platform_number; index++) {
-    if (!strcmp(id, platform_list[index].id)) {
+    if (!strcmp(id, platform_list[index]->id)) {
       return platform_list[index];
     }
   }
@@ -32,12 +32,12 @@ Platform get_platform_by_id(Platform *platform_list, size_t platform_number,
   abort();
 }
 
-Platform *get_platform_list_by_ids(Platform *platform_list,
-                                   size_t platform_number, char **connected_ids,
-                                   size_t ids_number) {
-  malloc_macro_def(Platform *, connected, sizeof(*connected));
+Platform **get_platform_list_by_ids(Platform **platform_list,
+                                    size_t platform_number,
+                                    char **connected_ids, size_t ids_number) {
+  malloc_macro_def(Platform **, connected, sizeof(*connected));
   for (size_t index = 0; index < ids_number; index++) {
-    realloc_macro(Platform *, connected, (index + 1) * sizeof(*connected));
+    realloc_macro(Platform **, connected, (index + 1) * sizeof(*connected));
     connected[index] = get_platform_by_id(platform_list, platform_number,
                                           connected_ids[index]);
   }
--- src/RBC/railway.c
+++ src/RBC/railway.c
@@ -32,13 +32,15 @@ Railway *get_malloc_railway_from(const char *file, const char *platform_delim,
   char **platform_list = get_malloc_token_list_number(
       railway_string, platform_delim, railway->platform_number);
   free(railway_string);
-  malloc_macro(Platform *, railway->platform_list,
-               sizeof(Platform) * railway->platform_number);
+  malloc_macro(Platform **, railway->platform_list,
+               sizeof(*railway->platform_list) * railway->platform_number);
   malloc_macro_def(char **, connected_id_list,
                    railway->platform_number * sizeof(*connected_id_list));
 
   for (size_t index = 0; index < railway->platform_number; index++) {
-    set_platform_from(&railway->platform_list[index], platform_list[index],
+    malloc_macro(Platform *, railway->platform_list[index],
+                 sizeof(*railway->platform_list[index]));
+    set_platform_from(railway->platform_list[index], platform_list[index],
                       platform_detail_delim, &connected_id_list[index]);
   }
   // Free not as array because values of array are used in railway
@@ -50,10 +52,10 @@ static Railway *connect_platforms(Railway *railway, char **id_list,
                                   const char *id_delim) {
   for (size_t index = 0; index < railway->platform_number; index++) {
     size_t ids_number = get_number_of_tokens(id_list[index], id_delim);
-    railway->platform_list[index].connected_number = ids_number;
+    railway->platform_list[index]->connected_number = ids_number;
     char **connected_ids =
         get_malloc_token_list_number(id_list[index], id_delim, ids_number);
-    railway->platform_list[index].connected = get_platform_list_by_ids(
+    railway->platform_list[index]->connected = get_platform_list_by_ids(
         railway->platform_list, railway->platform_number, connected_ids,
         ids_number);
     free(connected_ids);
@@ -74,9 +76,9 @@ bool can_transfer_on(const Platform platform) {
 //       get_malloc_railway_from("railway.txt", "\n", "<platform_data>", ",");
 //   // printf("%s,", railway->platform_list[0].connected[0].id);
 //   for (size_t i = 0; i < railway->platform_number; i++) {
-//     printf("%s\t: ", railway->platform_list[i].id);
-//     for (int j = 0; j < railway->platform_list[i].connected_number; j++) {
-//       printf("%s,", railway->platform_list[i].connected[j].id);
+//     printf("%s\t: ", railway->platform_list[i]->id);
+//     for (int j = 0; j < railway->platform_list[i]->connected_number; j++) {
+//       printf("%s,", railway->platform_list[i]->connected[j]->id);
 //       // printf("%d,", railway->platform_list[i].connected_number);
 //       // printf("%p,", railway->platform_list[i].connected[j]);
 //     }
