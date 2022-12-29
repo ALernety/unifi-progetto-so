6711ef2ab3748723cca2c1556e4e6cacc6a184cc
Aliaksei Shapiolkin <alernety@gmail.com>
2022-12-29 08:38:31 +0100

    Refactor log.c
    * Remove arrays with variable length
    * Use more safe snprintf instead sprintf
    * Free memory from malloced variables where possible
    * Use localtime_r instead localtime

--- src/common/log.c
+++ src/common/log.c
@@ -13,7 +13,7 @@
 
 static char *get_date(char *date_string, size_t *date_size);
 
-static char *log_rbc_format(Mode mode);
+static char *log_rbc_format_malloc(Mode mode);
 
 void log_segment(int fd, const char *current, const char *next)
 {
@@ -21,16 +21,19 @@ void log_segment(int fd, const char *current, const char *next)
 	const char *format_string = "[Attuale: %s], [Next: %s], %s\n";
 	char log_message_length = strlen(format_string) + strlen(current) +
 				  strlen(next) + date_size + 1;
-	char date[date_size];
+	malloc_macro_def(char *, date, date_size);
 	get_date(date, &date_size);
-	char log_message[log_message_length];
+	malloc_macro_def(char *, log_message, log_message_length);
 
-	sprintf(log_message, format_string, current, next, date);
+	snprintf(log_message, log_message_length, format_string, current, next,
+		 date);
 	int write_length = write(fd, log_message, strlen(log_message));
 	if (write_length != (int)strlen(log_message)) {
 		perror("log segment");
 		abort();
 	}
+	free(date);
+	free(log_message);
 }
 
 int log_create(char *log_file)
@@ -48,26 +51,30 @@ void log_rbc(int log_fd, const char *train, const char *current_platform,
 	     const char *request_platform, const bool permit, Mode mode)
 {
 	size_t date_size = 30;
-	const char *format_string = log_rbc_format(mode);
+	char *format_string = log_rbc_format_malloc(mode);
 	size_t log_length = strlen(format_string) + strlen(train) +
 			    strlen(current_platform) +
 			    strlen(request_platform) +
 			    /*authorize*/ 2 + date_size + 1;
-	char log_message[log_length];
+	malloc_macro_def(char *, log_message, log_length);
 
-	char date[date_size];
+	malloc_macro_def(char *, date, date_size);
 	get_date(date, &date_size);
 
-	sprintf(log_message, format_string, train, current_platform,
-		request_platform, permit ? "SI" : "NO", date);
+	snprintf(log_message, log_length, format_string, train,
+		 current_platform, request_platform, permit ? "SI" : "NO",
+		 date);
 	int write_length = write(log_fd, log_message, strlen(log_message));
 	if (write_length != (int)strlen(log_message)) {
 		perror("log segment");
 		abort();
 	}
+	free(date);
+	free(format_string);
+	free(log_message);
 }
 
-static char *log_rbc_format(Mode mode)
+static char *log_rbc_format_malloc(Mode mode)
 {
 	char *action_type;
 	switch (mode) {
@@ -88,12 +95,12 @@ static char *log_rbc_format(Mode mode)
 				    "[AUTORIZZATO: %%s], "
 				    "[DATA: %%s]\n";
 
-	size_t format_length = strlen(format_string) + strlen(action_type);
-	char format[format_length];
+	size_t format_length = strlen(format_string) + strlen(action_type) + 1;
+	malloc_macro_def(char *, format, format_length);
 
-	sprintf(format, format_string, action_type);
+	snprintf(format, format_length, format_string, action_type);
 	free(action_type);
-	return strdup(format);
+	return format;
 }
 
 static char *get_date(char *date_string, size_t *date_size)
@@ -103,7 +110,7 @@ static char *get_date(char *date_string, size_t *date_size)
 
 	static char *date_format = "%d/%m/%Y; %H:%M:%S";
 	now = time(NULL);
-	current_time = *(localtime(&now));
+	localtime_r(&now, &current_time);
 	*date_size =
 		strftime(date_string, *date_size, date_format, &current_time);
 	return date_string;
