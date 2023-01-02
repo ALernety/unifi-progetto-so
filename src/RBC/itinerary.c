6e463568871b627d0dda51805866b389cb9a5a50
Aliaksei Shapiolkin <alernety@gmail.com>
2023-01-02 22:30:54 +0100

    Fix count trains on stations and refactor
    * Add platform types
    * Add initial state of stations
    * Rename function can_transfer_on to can_enter_on to show what exactly it do
    * Remove unused comments

--- src/RBC/itinerary.c
+++ src/RBC/itinerary.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "RBC/platform.h"
 #include "common/alloc_macro.h"
 #include "common/string_handlers.h"
 
@@ -27,12 +28,19 @@ Itinerary *get_malloc_itinerary_from(char *itinerary_str, const char *delim,
 bool free_current_platform(Railway *railway, Itinerary *itinerary)
 {
 	char *current_id = itinerary->platform_ids[itinerary->current];
+	char *next_id = itinerary->platform_ids[itinerary->next];
 	if (is_on_last_platform_of(*itinerary)) {
 		return false;
 	}
 	Platform *current = get_platform_by_id(
 		railway->platform_list, railway->platform_number, current_id);
+	Platform *next = get_platform_by_id(current->connected,
+					    current->connected_number, next_id);
 	current->actual_capacity--;
+	// If is station permit not required.
+	if (next->type == STATION) {
+		next->actual_capacity++;
+	}
 	itinerary->current = itinerary->next;
 	if (itinerary->platform_number > itinerary->next + 1) {
 		itinerary->next++;
@@ -56,7 +64,7 @@ bool permit_to_next_platform(Railway *railway, Itinerary *itinerary,
 	// Check if platforms are connected, and get next platform if they are.
 	Platform *next = get_platform_by_id(current->connected,
 					    current->connected_number, next_id);
-	if (!can_transfer_on(*next)) {
+	if (!can_enter_on(*next)) {
 		return false;
 	}
 	next->actual_capacity++;
